986c3998a6961dd04ac2fdf316686af8f9551136
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 01:02:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,9 @@ def monopoly_with_third_tier
       three_pieces_owned: 100,
       four_pieces_owned: 200},
     names: {
-      reading_railroad: {}
+      reading_railroad: {},
+      pennsylvania_railroad: {},
+      
       }}}
 
 end
